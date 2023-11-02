3210998ff9ea21684a5bb60c25357326769a7a85
Lázaro Noel Guerra <lazaronoelg@yahoo.com>
2023-11-02 09:00:16 -0400

    Initial commit
    This is the initial commit for this repository

---
+++ drone-web-service/src/main/java/cu/repository/service/dronewebservice/DroneWebServiceApplication.java
@@ -0,0 +1,13 @@
+package cu.repository.service.dronewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneWebServiceApplication.class, args);
+    }
+
+}
---
+++ drone-web-service/src/test/java/cu/repository/service/dronewebservice/DroneWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cu.repository.service.dronewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
