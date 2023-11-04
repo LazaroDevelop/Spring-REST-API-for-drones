010b23a2cccfe35e1be5b74b03331e248a7e61b9
Lázaro Noel Guerra <lazaronoelg@yahoo.com>
2023-11-04 14:45:05 -0400

    add medication repository

--- drone-web-service/src/main/java/cu/repository/service/dronewebservice/repository/IMedicationRepository.java
+++ drone-web-service/src/main/java/cu/repository/service/dronewebservice/repository/IMedicationRepository.java
@@ -3,8 +3,8 @@ package cu.repository.service.dronewebservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import cu.repository.service.dronewebservice.model.entity.DroneEntity;
+import cu.repository.service.dronewebservice.model.entity.MedicationEntity;
 
 @Repository
-public interface IDroneRepository extends JpaRepository<DroneEntity, Long>{
+public interface IMedicationRepository extends JpaRepository<MedicationEntity, Long>{
 }
