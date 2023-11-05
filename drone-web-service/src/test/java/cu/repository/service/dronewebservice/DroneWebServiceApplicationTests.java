796a17bb30231c6fa79b945cec9659661ad63c27
Lázaro Noel Guerra <lazaronoelg@yahoo.com>
2023-11-05 11:35:33 -0500

    add unitary test

--- drone-web-service/src/test/java/cu/repository/service/dronewebservice/DroneWebServiceApplicationTests.java
+++ drone-web-service/src/test/java/cu/repository/service/dronewebservice/DroneWebServiceApplicationTests.java
@@ -1,13 +1,75 @@
 package cu.repository.service.dronewebservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import cu.repository.service.dronewebservice.model.entity.DroneEntity;
+import cu.repository.service.dronewebservice.model.entity.MedicationEntity;
+import cu.repository.service.dronewebservice.model.enums.EModels;
+import cu.repository.service.dronewebservice.model.enums.EState;
+import cu.repository.service.dronewebservice.repository.IDroneRepository;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest(DroneWebServiceApplicationTests.class)
 class DroneWebServiceApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    IDroneRepository droneRepository;
+
     @Test
-    void contextLoads() {
+    void pingTest() {
+        this.webTestClient
+                .get()
+                .uri("/v1/api/ping")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(String.class)
+                .isEqualTo("The service is running");
+    }
+
+    @Test
+    void createNewDroneWithTestClient() {
+        DroneEntity drone = new DroneEntity();
+        drone.setBattery(80);
+        drone.setMedications(new HashSet<MedicationEntity>());
+        drone.setModel(EModels.CRUISERWEIGHT);
+        drone.setWeight(498.2);
+        drone.setState(EState.IDLE);
+
+        this.webTestClient.post()
+                .uri("/v1/api/drone-new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(drone)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(DroneEntity.class).isEqualTo(drone);
+
+    }
+
+    @Test
+    void createNewDrone() {
+        DroneEntity drone = new DroneEntity();
+        drone.setBattery(80);
+        drone.setMedications(new HashSet<MedicationEntity>());
+        drone.setModel(EModels.CRUISERWEIGHT);
+        drone.setWeight(498.2);
+        drone.setState(EState.IDLE);
+
+        assertEquals(this.droneRepository.save(drone), drone);
     }
 
 }
