feb503fb791578fdaa739d375c37f841654fbf24
Lázaro Noel Guerra <lazaronoelg@yahoo.com>
2023-11-05 11:35:13 -0500

    add scheduled task for log event support

--- drone-web-service/src/main/java/cu/repository/service/dronewebservice/DroneWebServiceApplication.java
+++ drone-web-service/src/main/java/cu/repository/service/dronewebservice/DroneWebServiceApplication.java
@@ -2,10 +2,11 @@ package cu.repository.service.dronewebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DroneWebServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DroneWebServiceApplication.class, args);
     }
--- drone-web-service/src/main/java/cu/repository/service/dronewebservice/service/CheckBatteryService.java
+++ drone-web-service/src/main/java/cu/repository/service/dronewebservice/service/CheckBatteryService.java
@@ -3,18 +3,21 @@ package cu.repository.service.dronewebservice.service;
 import java.util.Date;
 import java.util.List;
 
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+
 
 import cu.repository.service.dronewebservice.model.entity.CheckBatteryEvent;
 import cu.repository.service.dronewebservice.model.entity.DroneEntity;
 import cu.repository.service.dronewebservice.repository.IBatteryEventRepository;
 import cu.repository.service.dronewebservice.repository.IDroneRepository;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Service
+@Component
 public class CheckBatteryService implements ICheckBatteryService{
 
     @Autowired
@@ -23,12 +26,15 @@ public class CheckBatteryService implements ICheckBatteryService{
     @Autowired
     IBatteryEventRepository batteryEventRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(CheckBatteryService.class);
+
     @Override
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(cron = "*/10 * * * * *")
     public void monitoringDroneBatteryLevel() {
         List<DroneEntity> drones = this.droneRepository.findAll();
+        // CheckBatteryService.logger.info("Elements: {}", drones.size());
         drones.forEach(d -> {
-            log.info("Checking drones battery, drone: {}", d.toString());
+            CheckBatteryService.logger.info("Drone with serial number: {} and model:{} has {}% of battery level", d.getSerialNumber(), d.getModel(), d.getBattery());
             CheckBatteryEvent batteryEvent = new CheckBatteryEvent();
             batteryEvent.setDroneSerialNumber(d.getSerialNumber());
             batteryEvent.setDroneBatteryLevel(d.getBattery());
